11a18d38857a5d3dd79f0f3d8a14d484acbc4be2
AUbillaOliva <afubillaoliva@gmail.com>
2020-07-12 11:08:21 -0400

    :sparkles: initializing mongo-auto-increment

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const autoincrement = require('mongoose-auto-increment');
+const mongoose = require('mongoose');
 
 const connectDB  = require('./config/db.js');
 
 connectDB();
+autoincrement.initialize(mongoose.connection);
 
 const PORT = process.env.PORT || 5000;
 
