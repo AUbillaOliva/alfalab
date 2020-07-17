f99f029ef4974ad4478ad7e5be08b997efef37c4
AUbillaOliva <afubillaoliva@gmail.com>
2020-07-17 07:06:15 -0400

    :hammer: code refactored

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongoUri');
+const db = config.get("mongoUri");
 
 const connectDB = async (req, res) => {
   try {
