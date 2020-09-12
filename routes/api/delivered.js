8af8d763fb1fa7e740726fd8d854e99cc85061e8
Álvaro Ubilla Oliva <afubillaoliva@gmail.com>
2020-09-11 21:50:45 -0300

    :zap: General route update

--- routes/api/delivered.js
+++ routes/api/delivered.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
-const OrderSchema = require('../../modules/Orders');
-const autoincrement = require('mongoose-auto-increment');
+const OrdersSchema = require('../../modules/Orders');
 const mongoose = require('mongoose');
-OrderSchema.plugin(autoincrement.plugin, { model: 'orders', field: 'number', startAt: 0, incrementBy: 1 });
 
-const Delivered = mongoose.model('delivered', OrderSchema);
+const Delivered = mongoose.model('delivered', OrdersSchema);
 
 router.get('/', async (req, res) => {
   try {
@@ -19,81 +17,85 @@ router.get('/', async (req, res) => {
       res.send(data);
     }
   } catch(err){
+    process.stdout.write('\033c');
     console.error(err);
   }
 });
 
-router.post('/', 
+router.post('/:number?', 
+[
   [
-    [
-      check('responsible').not().isEmpty(),
-      check('checkin').not().isEmpty(),
-      check('checkout').not().isEmpty(),
-      check('client.firstname').not().isEmpty(),
-      check('client.lastname').not().isEmpty(),
-      check('quantity').not().isEmpty()
-    ]
-  ], async (req, res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-      return res.status(400).json({errors: errors.array()});
-    }
+    check('order').not().isEmpty(),
+    check('client.firstname').not().isEmpty(),
+    check('client.lastname').not().isEmpty(),
+    check('created_at').not().isEmpty(),
+    check('status').not().isEmpty()
+  ]
+], async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    process.stdout.write('\033c');
+    return res.status(400).json({errors: errors.array()});
+  }
 
-    const {
-      commentaries,
-      responsible,
-      checkin,
-      checkout,
-      orderType,
-      status,
-      price,
-      client,
-      quantity
-    } = req.body;
+  const {
+    order,
+    client,
+    orders_number,
+    zone,
+    delivered_by,
+    created_at,
+    delivered_date,
+    total,
+    status
+  } = req.body;
 
-    const deliveryFields = {};    
-    if(commentaries){ deliveryFields.commentaries = commentaries }
-    if(checkin){ deliveryFields.checkin = checkin; }
-    if(checkout) { deliveryFields.checkout = checkout; }
-    if(orderType) { deliveryFields.orderType = orderType; }
-    if(status) { deliveryFields.status = status; }
-    if(price) { deliveryFields.price = price; }
-    if(client) { deliveryFields.client = client; }
-    if(responsible) { deliveryFields.responsible = responsible; } 
-    if(quantity) { deliveryFields.quantity = quantity; }
-    
-    try {
-      let delivery = await Delivered.findOne(deliveryFields);
-      if(delivery) {
-        delivery = await Delivered.findOneAndUpdate({
-          $set: deliveryFields,
-          new: true
-        });
-        console.log(`Delivery ${delivery._id} updated`);
-        res.send(delivery).status(200);
-      } else {
-        delivery = new Delivered(deliveryFields);
-        await delivery.save();
-        console.log(`Delivery ${delivery._id} created`);
-        res.send(delivery).status(200);
-      }
-    } catch(err){
-      console.error(err);
-      res.status(500).send('server error');
+  const deliveryFields = {};    
+  if(order) { deliveryFields.order = order; }
+  if(client) { deliveryFields.client = client; }
+  if(zone) { deliveryFields.zone = zone; }
+  if(delivered_by) { deliveryFields.delivered_by = delivered_by; }
+  if(orders_number) { deliveryFields.orders_number = orders_number; }
+  if(created_at) { deliveryFields.created_at = created_at; }
+  if(delivered_date) { deliveryFields.delivered_date = delivered_date; }
+  if(total) { deliveryFields.total = total; }
+  if(status != null || status != undefined) { deliveryFields.status = status; }
+  
+  try {
+    let delivery = await Delivered.findOne({orders_number: req.params.number});
+    if(delivery) {
+      delivery = await Delivered.findOneAndUpdate({
+        $set: deliveryFields,
+        new: true
+      });
+      console.log(`Delivery ${delivery._id} updated`);
+      res.send(delivery).status(200);
+    } else {
+      delivery = new Delivered(deliveryFields);
+      await delivery.save();
+      console.log(`Delivery ${delivery._id} created`);
+      res.send(delivery).status(200);
     }
+  } catch(err){
+    process.stdout.write('\033c');
+    console.error(err);
+    res.status(500).send('server error');
+  }
 });
 
 router.delete('/:number', async (req, res) => {
   try {
-    let delivery = await Delivered.findOne({ number: req.params.number });
+    let delivery = await Delivered.findOne({ orders_number: req.params.number });
     if(delivery){
       await Delivered.findByIdAndRemove(delivery._id);
       console.log(`Delivery ${delivery._id} deleted`);
       res.send(`Delivery ${delivery._id} deleted`).status(200);
     } else {
-      res.send('Order not found').status(404);
+      res.send('Delivery not found').status(404);
+      console.log('Delivery not found');
     }
   } catch (err) {
+    process.stdout.write('\033c');
     console.error(err);
     res.send('Server Error').status(500);
   }
