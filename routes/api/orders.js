cf1673bb638dde8c7619a89274c6fb7acace9430
Álvaro Ubilla Oliva <afubillaoliva@gmail.com>
2020-09-11 21:51:37 -0300

    :zap: General route update

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
-const OrderSchema = require('../../modules/Orders');
+const OrdersSchema = require('../../modules/Orders');
 const mongoose = require('mongoose');
 const autoincrement = require('mongoose-auto-increment');
 
-
-OrderSchema.plugin(autoincrement.plugin, { model: 'orders', field: 'number', startAt: 0, incrementBy: 1 });
-const Order = mongoose.model('orders', OrderSchema);
+OrdersSchema.plugin(autoincrement.plugin, { model: 'orders', field: 'orders_number', startAt: 0, incrementBy: 1 });
+const Orders = mongoose.model('orders', OrdersSchema);
 
 router.get('/', async (req, res) => {
   try {
-    const data = await Order.find().populate('data');
+    const data = await Orders.find().populate('data');
     if(!data){
       res.status(404).send({
         msg: 'There is not orders'
@@ -20,6 +19,7 @@ router.get('/', async (req, res) => {
       res.send(data);
     }
   } catch(err){
+    process.stdout.write('\033c');
     console.error(err);
   }
 });
@@ -27,57 +27,57 @@ router.get('/', async (req, res) => {
 router.post('/:number?', 
   [
     [
-      check('responsible').not().isEmpty(),
-      check('checkin').not().isEmpty(),
+      check('order').not().isEmpty(),
       check('client.firstname').not().isEmpty(),
       check('client.lastname').not().isEmpty(),
-      check('quantity').not().isEmpty()
+      check('created_at').not().isEmpty(),
+      check('status').not().isEmpty()
     ]
   ], async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
+      process.stdout.write('\033c');
       return res.status(400).json({errors: errors.array()});
     }
 
     const {
-      commentaries,
-      responsible,
-      checkin,
-      checkout,
-      orderType,
-      status,
-      price,
+      order,
       client,
-      quantity
+      zone,
+      delivered_by,
+      created_at,
+      delivered_date,
+      total,
+      status
     } = req.body;
 
-    const orderFields = {};    
-    if(commentaries){ orderFields.commentaries = commentaries }
-    if(checkin){ orderFields.checkin = checkin; }
-    if(checkout) { orderFields.checkout = checkout; }
-    if(orderType) { orderFields.orderType = orderType; }
-    if(status) { orderFields.status = status; }
-    if(price) { orderFields.price = price; }
-    if(client) { orderFields.client = client; }
-    if(responsible) { orderFields.responsible = responsible; } 
-    if(quantity) { orderFields.quantity = quantity; }
+    const ordersFields = {};    
+    if(order) { ordersFields.order = order; }
+    if(client) { ordersFields.client = client; }
+    if(zone) { ordersFields.zone = zone; }
+    if(delivered_by) { ordersFields.delivered_by = delivered_by; }
+    if(created_at) { ordersFields.created_at = created_at; }
+    if(delivered_date) { ordersFields.delivered_date = delivered_date; }
+    if(total) { ordersFields.total = total; }
+    if(status != null || status != undefined) { ordersFields.status = status; }
     
     try {
-      let order = await Order.findOne({number: req.params.number});
-      if(order) {
-        order = await Order.findOneAndUpdate({
-          $set: orderFields,
+      let orders = await Orders.findOne({orders_number: req.params.number});
+      if(orders) {
+        orders = await Orders.findOneAndUpdate({
+          $set: ordersFields,
           new: true
         });
-        console.log(`Order ${order._id} updated`);
-        res.send(order).status(200);
+        console.log(`Orders ${orders._id} updated`);
+        res.send(orders).status(200);
       } else {
-        order = new Order(orderFields);
-        await order.save();
-        console.log(`Order ${order._id} created`);
-        res.send(order).status(200);
+        orders = new Orders(ordersFields);
+        await orders.save();
+        console.log(`Orders ${orders._id} created`);
+        res.send(orders).status(200);
       }
     } catch(err){
+      process.stdout.write('\033c');
       console.error(err);
       res.status(500).send('server error');
     }
@@ -85,16 +85,16 @@ router.post('/:number?',
 
 router.delete('/:number', async (req, res) => {
   try {
-    let order = await Order.findOne({ number: req.params.number });
-    console.log(order);
-    if(order){
-      await Order.findByIdAndRemove(order._id);
-      console.log(`Order ${order._id} deleted`);
-      res.send(`Order ${order._id} deleted`).status(200);
+    let orders = await Orders.findOne({ orders_number: req.params.number });
+    if(orders){
+      await Orders.findByIdAndRemove(orders._id);
+      console.log(`Orders ${orders._id} deleted`);
+      res.send(`Orders ${orders._id} deleted`).status(200);
     } else {
-      res.send('Order not found').status(404);
+      res.send('Orders not found').status(404);
     }
   } catch (err) {
+    process.stdout.write('\033c');
     console.error(err);
   }
 });
