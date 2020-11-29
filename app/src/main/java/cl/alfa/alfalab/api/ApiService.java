0146ddc24e02c5cb24f4969588284479ce23e338
Álvaro Ubilla Oliva <afubillaoliva@gmail.com>
2020-11-29 10:27:58 -0300

    :sparkles: authentication functionality added

--- app/src/main/java/cl/alfa/alfalab/api/ApiService.java
+++ app/src/main/java/cl/alfa/alfalab/api/ApiService.java
@@ -1,15 +1,25 @@
 package cl.alfa.alfalab.api;
 
 import java.util.ArrayList;
+import java.util.Map;
 
+import cl.alfa.alfalab.models.LoginData;
 import cl.alfa.alfalab.models.Orders;
+import cl.alfa.alfalab.models.User;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public class ApiService {
 
@@ -25,7 +35,12 @@ public class ApiService {
 
     public interface PostOrderService {
         @POST("orders")
-        Call<Orders> postOrder(@Body Orders order);
+        Call<Orders> postOrder(@Body Orders order, @Header("x-auth-token") String token);
+    }
+
+    public interface GetUserService {
+        @GET("auth")
+        Call<User> getUser(@Header("x-auth-token") String token);
     }
 
     public interface UpdateOrderService {
@@ -47,4 +62,15 @@ public class ApiService {
         @POST("delivered")
         Call<Orders> setDelivered(@Body Orders order);
     }
+
+    public interface GetAuthService {
+        @POST("auth")
+        Call<ResponseBody> getAuth(@Body LoginData loginData);
+    }
+
+    public interface SignUpService {
+        @POST("users")
+        Call<ResponseBody> signUp(@Body User user);
+    }
+
 }
