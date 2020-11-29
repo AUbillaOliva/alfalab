a9806cd6d99a3fd7f3f44de3176bd337dde0232a
Álvaro Ubilla Oliva <afubillaoliva@gmail.com>
2020-11-29 07:45:07 -0300

    :sparkles: LoginData model added

--- app/src/main/java/cl/alfa/alfalab/models/LoginData.java
+++ app/src/main/java/cl/alfa/alfalab/models/LoginData.java
@@ -1,4 +1,27 @@
 package cl.alfa.alfalab.models;
 
 public class LoginData {
+    private String email;
+    private String password;
+
+    public LoginData(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
