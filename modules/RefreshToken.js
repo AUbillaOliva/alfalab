e26378e0db716461b01578395ae8fdd29fa333c9
AUbillaOliva <afubillaoliva@gmail.com>
2020-12-29 22:52:57 -0300

    :art: Code reformated

--- modules/RefreshToken.js
+++ modules/RefreshToken.js
@@ -1,44 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const schema = new Schema({
+const schema = new Schema(
+  {
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user'
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
     },
     token: {
-        type: String,
-        required: true 
+      type: String,
+      required: true,
     },
     expires: {
-        type: Date,
-        required: false
+      type: Date,
+      required: false,
     },
     created: {
-        type: Date,
-        default: Date.now(),
-        required: true
+      type: Date,
+      default: Date.now(),
+      required: true,
     },
     revoked: {
-        type: Date,
-        required: false
+      type: Date,
+      required: false,
     },
     expiredToken: {
-        type: String,
-        required: false
-    }
-},
-{
+      type: String,
+      required: false,
+    },
+  },
+  {
     toObject: { virtuals: true },
-    toJSON: { virtuals: true }
+    toJSON: { virtuals: true },
+  }
+);
+
+schema.virtual("isExpired").get(function () {
+  return Date.now() >= this.expires;
 });
 
-schema.virtual('isExpired').get(function () {
-    return Date.now() >= this.expires;
+schema.virtual("isActive").get(function () {
+  return !this.revoked && !this.isExpired;
 });
 
-schema.virtual('isActive').get(function () {
-    return !this.revoked && !this.isExpired;
-});
-
-module.exports = mongoose.model('RefreshToken', schema);
+module.exports = mongoose.model("RefreshToken", schema);
