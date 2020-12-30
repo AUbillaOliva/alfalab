f462171f4dbf53b7678b857e7bd1fdf1d3ba79b1
AUbillaOliva <afubillaoliva@gmail.com>
2020-12-29 22:48:53 -0300

    :sparkles: Added messages route; :art: Code reformated

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-require('newrelic');
-require('dotenv').config();
-const express = require('express');
+require("newrelic");
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const autoincrement = require('mongoose-auto-increment');
-const mongoose = require('mongoose');
+const autoincrement = require("mongoose-auto-increment");
+const mongoose = require("mongoose");
 
-const connectDB  = require('./database/db.js');
+const connectDB = require("./database/db.js");
 
 connectDB();
 autoincrement.initialize(mongoose.connection);
@@ -14,13 +14,18 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use('/api/delivered', require('./routes/api/delivered'));
-app.use('/api/orders', require('./routes/api/orders'));
-app.use('/support/report', require('./routes/support/report'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/users', require('./routes/api/users'));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.use("/api/delivered", require("./routes/api/delivered"));
+app.use("/api/orders", require("./routes/api/orders"));
+app.use("/support/report", require("./routes/support/report"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/messages", require("./routes/api/messages"));
 
 app.listen(PORT, () => {
-  process.stdout.write('\033c');
+  process.stdout.write("\033c");
   console.log(`~ Server running on port ${PORT}`);
-})
+});
