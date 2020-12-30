db781ca82daed9a715931ede0e45f74fe04691ca
AUbillaOliva <afubillaoliva@gmail.com>
2020-12-29 22:51:36 -0300

    :art: Code reformated

--- modules/Clients.js
+++ modules/Clients.js
@@ -1,24 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const ClientSchema = new mongoose.Schema({
   firstname: {
     type: String,
     required: true,
-    lowercase: true
+    lowercase: true,
   },
   lastname: {
     type: String,
     required: true,
-    lowercase: true
+    lowercase: true,
   },
   instagram: {
     type: String,
-    lowercase: true
+    lowercase: true,
   },
   email: {
     type: String,
-    lowercase: true
-  }
+    lowercase: true,
+  },
 });
 
 module.exports = ClientSchema;
