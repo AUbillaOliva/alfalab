fcc3c14a0d091940ccd6fcd3cd122539a82f1cf5
AUbillaOliva <afubillaoliva@gmail.com>
2020-12-29 22:51:05 -0300

    :art: Code reformated

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,20 @@
-const jwt = require('jsonwebtoken');
-const httpError = require('http-errors');
+const jwt = require("jsonwebtoken");
+const httpError = require("http-errors");
 
-module.exports = function(req, res, next){
-    const token = req.header('x-auth-token');
-    if(!token) { 
-        res.status(401).json(httpError.Unauthorized('No token provided, Unauthorized'));
-    } else {
-        try {
-            const decoded = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
-            req.user = decoded.user;
-            next();
-        } catch(err) {
-            console.log(err.message);
-            next(httpError.InternalServerError('Server error'));
-        }
+module.exports = function (req, res, next) {
+  const token = req.header("x-auth-token");
+  if (!token) {
+    res
+      .status(401)
+      .json(httpError.Unauthorized("No token provided, Unauthorized"));
+  } else {
+    try {
+      const decoded = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
+      req.user = decoded.user;
+      next();
+    } catch (err) {
+      console.log(err.message);
+      next(httpError.InternalServerError("Server error"));
     }
-}
-
-
+  }
+};
