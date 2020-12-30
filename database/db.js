e98765511718b5b3dc92679aa45d1a85de1063a5
AUbillaOliva <afubillaoliva@gmail.com>
2020-12-29 22:47:12 -0300

    :art: Code reformated

--- database/db.js
+++ database/db.js
@@ -1,21 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const db = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true
-    }).then(() => {
-      console.log('Database connected');
-    }).catch((error) => {
-      console.error(error.message);
-    });
-  } catch(error) {
+    await mongoose
+      .connect(db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+      })
+      .then(() => {
+        console.log("Database connected");
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  } catch (error) {
     console.error(error.message);
   }
-}
+};
 
 module.exports = connectDB;
